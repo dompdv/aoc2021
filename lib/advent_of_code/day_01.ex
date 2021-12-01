c31debcb9af6f0cbc031220bef4d5b42dfa6ecce
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2021-12-01 09:34:36 +0100

    d01 formatted

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,13 +1,19 @@
 defmodule AdventOfCode.Day01 do
   import Enum
+
   def part1(args) do
-    input = [_ | t] = args |> String.split("\n") |> drop(-1) |> map(fn v -> String.to_integer(v) end)
-    Enum.zip([t, input]) |> map(fn {h,l}-> h-l end) |> filter(&(&1 > 0)) |> count()
+    input =
+      [_ | t] = args |> String.split("\n") |> drop(-1) |> map(fn v -> String.to_integer(v) end)
+
+    Enum.zip([t, input]) |> map(fn {h, l} -> h - l end) |> filter(&(&1 > 0)) |> count()
   end
 
   def part2(args) do
-    input = [a, b, c | t] = args |> String.split("\n") |> drop(-1) |> map(fn v -> String.to_integer(v) end)
-    input = [_ | t] = Enum.zip([input, [b,c|t],[c|t]]) |> map(fn {a,b,c} -> a+b+c end)
-    Enum.zip([t, input]) |> map(fn {h,l}-> h-l end) |> filter(&(&1 > 0)) |> count()
+    input =
+      [_a, b, c | t] =
+      args |> String.split("\n") |> drop(-1) |> map(fn v -> String.to_integer(v) end)
+
+    input = [_ | t] = Enum.zip([input, [b, c | t], [c | t]]) |> map(fn {a, b, c} -> a + b + c end)
+    Enum.zip([t, input]) |> map(fn {h, l} -> h - l end) |> filter(&(&1 > 0)) |> count()
   end
 end
