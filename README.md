6ab30c39e1048073db38b08881c03d6c70ba61e1
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2021-12-01 09:18:34 +0100

    d01 p1

--- README.md
+++ README.md
@@ -13,10 +13,10 @@ There are 25 modules, 25 tests, and 50 mix tasks.
 
 ```elixir
 defmodule AdventOfCode.Day01 do
-  def part1(args) do
+  def part1(_args) do
   end
 
-  def part2(args) do
+  def part2(_args) do
   end
 end
 ```
--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,10 @@
 defmodule AdventOfCode.Day01 do
+  import Enum
   def part1(args) do
+    input = [_ | t] = args |> String.split("\n") |> drop(-1) |> map(fn v -> String.to_integer(v) end)
+    Enum.zip([t, input]) |> map(fn {h,l}-> h-l end) |> filter(&(&1 > 0)) |> count()
   end
 
-  def part2(args) do
+  def part2(_args) do
   end
 end
