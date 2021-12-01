e02703bb103bc62510ebde7e5fb76d57ecb620f5
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2021-12-01 09:33:12 +0100

    d01 p2

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -5,6 +5,9 @@ defmodule AdventOfCode.Day01 do
     Enum.zip([t, input]) |> map(fn {h,l}-> h-l end) |> filter(&(&1 > 0)) |> count()
   end
 
-  def part2(_args) do
+  def part2(args) do
+    input = [a, b, c | t] = args |> String.split("\n") |> drop(-1) |> map(fn v -> String.to_integer(v) end)
+    input = [_ | t] = Enum.zip([input, [b,c|t],[c|t]]) |> map(fn {a,b,c} -> a+b+c end)
+    Enum.zip([t, input]) |> map(fn {h,l}-> h-l end) |> filter(&(&1 > 0)) |> count()
   end
 end
