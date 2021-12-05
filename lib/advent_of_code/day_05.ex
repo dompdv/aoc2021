ebcacfe7f267bacba3b99658b2c7d86617589c9e
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2021-12-05 11:28:13 +0100

    day5 p2

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -1,6 +1,4 @@
 defmodule AdventOfCode.Day05 do
-  import Enum
-
   def parse_line(line) do
     [s, e] = String.split(line, " -> ")
     [x1, y1] = String.split(s, ",")
@@ -10,36 +8,57 @@ defmodule AdventOfCode.Day05 do
 
   def dangerous({x, y}, lines) do
     lines
-    |> map(fn {x1, y1, x2, y2} ->
-      cond do
-        y1 == y2 and y == y1 ->
-          if Kernel.min(x1, x2) <= x and x <= Kernel.max(x1, x2), do: 1, else: 0
-
-        x1 == x2 and x == x1 ->
-          if Kernel.min(y1, y2) <= y and y <= Kernel.max(y1, y2), do: 1, else: 0
-
-        true ->
-          0
-      end
+    |> Enum.map(fn {x1, y1, x2, y2} ->
+      if min(x1, x2) <= x and x <= max(x1, x2) and min(y1, y2) <= y and y <= max(y1, y2),
+        do: 1,
+        else: 0
     end)
-    |> sum()
+    |> Enum.sum()
   end
 
   def part1(args) do
     lines =
       args
       |> String.split("\n", trim: true)
-      |> map(&parse_line/1)
-      |> filter(fn {x1, y1, x2, y2} -> x1 == x2 or y1 == y2 end)
+      |> Enum.map(&parse_line/1)
+      |> Enum.filter(fn {x1, y1, x2, y2} -> x1 == x2 or y1 == y2 end)
 
-    max_x = lines |> map(fn {x1, _y1, x2, _y2} -> Kernel.max(x1, x2) end) |> max()
-    max_y = lines |> map(fn {_x1, y1, _x2, y2} -> Kernel.max(y1, y2) end) |> max()
+    max_x = lines |> Enum.map(fn {x1, _y1, x2, _y2} -> max(x1, x2) end) |> Enum.max()
+    max_y = lines |> Enum.map(fn {_x1, y1, _x2, y2} -> max(y1, y2) end) |> Enum.max()
 
     for(row <- 0..max_y, col <- 0..max_x, do: {col, row})
-    |> filter(fn coordinates -> dangerous(coordinates, lines) >= 2 end)
-    |> count()
+    |> Enum.filter(fn coordinates -> dangerous(coordinates, lines) >= 2 end)
+    |> Enum.count()
   end
 
-  def part2(_args) do
+  def on_line({x, y}, {x1, y1, x2, y2})
+      when (x < x1 and x < x2) or (x > x1 and x > x2) or (y < y1 and y < y2) or
+             (y > y1 and y > y2),
+      do: 0
+
+  def on_line({x, y}, {x1, y1, x2, y2}) do
+    cond do
+      (x == x1 and y == y1) or (x == x2 and y == y2) -> 1
+      y1 == y2 or x1 == x2 -> 1
+      y == y1 -> if (y - y1) / (x - x1) == (y2 - y1) / (x2 - x1), do: 1, else: 0
+      true -> if (x - x1) / (y - y1) == (x2 - x1) / (y2 - y1), do: 1, else: 0
+    end
+  end
+
+  def on_lines(coord, lines),
+    do: lines |> Enum.map(fn line -> on_line(coord, line) end) |> Enum.sum()
+
+  def part2(args) do
+    lines =
+      args
+      |> String.split("\n", trim: true)
+      |> Enum.map(&parse_line/1)
+
+    max_x = lines |> Enum.map(fn {x1, _y1, x2, _y2} -> max(x1, x2) end) |> Enum.max()
+    max_y = lines |> Enum.map(fn {_x1, y1, _x2, y2} -> max(y1, y2) end) |> Enum.max()
+
+    for(row <- 0..max_y, col <- 0..max_x, do: {col, row})
+    |> Enum.filter(fn coordinates -> on_lines(coordinates, lines) >= 2 end)
+    |> Enum.count()
   end
 end
--- lib/mix/tasks/d05.p2.ex
+++ lib/mix/tasks/d05.p2.ex
@@ -5,7 +5,19 @@ defmodule Mix.Tasks.D05.P2 do
 
   @shortdoc "Day 05 Part 2"
   def run(args) do
-    input = nil
+    # AdventOfCode.Input.get!(5, 2021)
+    input = """
+    0,9 -> 5,9
+    8,0 -> 0,8
+    9,4 -> 3,4
+    2,2 -> 2,1
+    7,0 -> 7,4
+    6,4 -> 2,0
+    0,9 -> 2,9
+    3,4 -> 1,4
+    0,0 -> 8,8
+    5,5 -> 8,2
+    """
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_2: fn -> input |> part2() end}),
