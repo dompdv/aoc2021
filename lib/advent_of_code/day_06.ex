a9875b1f76b6cf989e64814a1db3a9b8f2f19593
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2021-12-06 12:07:38 +0100

    day06 nicer

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,25 +1,7 @@
 defmodule AdventOfCode.Day06 do
   import Enum
 
-  def one_generation(_, fishes), do: one_gen(fishes, [])
-
-  def one_gen([], acc), do: acc
-
-  def one_gen([fish | tail], acc) do
-    case fish do
-      0 -> one_gen(tail, [8, 6 | acc])
-      _ -> one_gen(tail, [fish - 1 | acc])
-    end
-  end
-
-  def part1(args) do
-    fishes = args |> String.trim() |> String.split(",", trim: true) |> map(&String.to_integer/1)
-
-    reduce(1..80, fishes, &one_generation/2)
-    |> count()
-  end
-
-  def part2(args) do
+  def compute_generations(args, n_generations) do
     fishes =
       args
       |> String.trim()
@@ -27,8 +9,9 @@ defmodule AdventOfCode.Day06 do
       |> map(&String.to_integer/1)
       |> frequencies()
 
-    reduce(1..256, fishes, fn _, acc ->
-      for i <- 8..0 do
+    1..n_generations
+    |> reduce(fishes, fn _, acc ->
+      for i <- 0..8 do
         if i == 0,
           do: [{6, Map.get(acc, 0, 0)}, {8, Map.get(acc, 0, 0)}],
           else: {i - 1, Map.get(acc, i, 0)}
@@ -39,4 +22,7 @@ defmodule AdventOfCode.Day06 do
     |> Map.values()
     |> sum()
   end
+
+  def part1(args), do: compute_generations(args, 80)
+  def part2(args), do: compute_generations(args, 256)
 end
