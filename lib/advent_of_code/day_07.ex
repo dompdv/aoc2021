4fee5ab2e715dcb5458faeefeed96d479eca4b85
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2021-12-07 09:25:54 -0500

    day07 enfin pas trop moche

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -13,34 +13,22 @@ defmodule AdventOfCode.Day07 do
     {crabs, min_pos, max_pos}
   end
 
-  def fuel_to(pos, crabs), do: for({p, n} <- crabs, do: abs(p - pos) * n) |> sum()
-
-  def part1(args) do
-    {crabs, min_pos, max_pos} = parse(args)
-
+  def find_minimum({crabs, min_pos, max_pos}, distance) do
     min_pos..(max_pos - 1)
     |> reduce(
-      fuel_to(max_pos, crabs),
+      distance.(max_pos, crabs),
       fn pos, current_min ->
-        fuel_to_pos = fuel_to(pos, crabs)
+        fuel_to_pos = distance.(pos, crabs)
         if fuel_to_pos < current_min, do: fuel_to_pos, else: current_min
       end
     )
   end
+
+  def fuel_to(pos, crabs), do: for({p, n} <- crabs, do: abs(p - pos) * n) |> sum()
 
   def fuel_to2(pos, crabs),
     do: for({p, n} <- crabs, do: n * div(abs(p - pos) * (abs(p - pos) + 1), 2)) |> sum()
 
-  def part2(args) do
-    {crabs, min_pos, max_pos} = parse(args)
-
-    min_pos..(max_pos - 1)
-    |> reduce(
-      fuel_to2(max_pos, crabs),
-      fn pos, current_min ->
-        fuel_to_pos = fuel_to2(pos, crabs)
-        if fuel_to_pos < current_min, do: fuel_to_pos, else: current_min
-      end
-    )
-  end
+  def part1(args), do: parse(args) |> find_minimum(&fuel_to/2)
+  def part2(args), do: parse(args) |> find_minimum(&fuel_to2/2)
 end
