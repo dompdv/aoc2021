8f96e6a36247db296462925712b5f414a7e2e573
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2021-12-08 06:36:55 -0500

    d08 p1

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -1,7 +1,25 @@
 defmodule AdventOfCode.Day08 do
-  def part1(_args) do
+  import Enum
+
+  def parse_line(line) do
+    [left, right] = line |> String.split(" | ", trim: true)
+    digits = right |> String.split(" ", trim: true) |> map(&to_charlist/1) |> map(&sort/1)
+    clues = left |> String.split(" ", trim: true) |> map(&to_charlist/1) |> map(&sort/1)
+    {clues, digits}
   end
 
-  def part2(_args) do
+  def parse(args), do: args |> String.split("\n", trim: true) |> map(&parse_line/1)
+
+  def part1(args) do
+    parse(args)
+    |> map(fn {_, digits} ->
+      digits |> map(&count/1) |> filter(fn c -> member?([2, 4, 3, 7], c) end) |> count()
+    end)
+    |> sum()
   end
+
+  def process_line({clues, digits}) do
+    digits
+  end
+  def part2(args), do: parse(args) |> map(&process_line/1)
 end
--- lib/mix/tasks/d08.p1.ex
+++ lib/mix/tasks/d08.p1.ex
@@ -5,7 +5,22 @@ defmodule Mix.Tasks.D08.P1 do
 
   @shortdoc "Day 08 Part 1"
   def run(args) do
-    input = nil
+    #
+    input = AdventOfCode.Input.get!(8, 2021)
+
+    # "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf\n"
+    """
+    be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+    edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+    fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+    fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+    aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+    fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+    dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+    bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+    egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+    gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+    """
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_1: fn -> input |> part1() end}),
