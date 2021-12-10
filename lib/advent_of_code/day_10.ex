5ca9b73f8c224cfbbe7bb7e955fc92f64770f31c
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2021-12-10 05:06:53 -0500

    day10

--- lib/advent_of_code/day_10.ex
+++ lib/advent_of_code/day_10.ex
@@ -4,6 +4,7 @@ defmodule AdventOfCode.Day10 do
   @opening [?(, ?{, ?[, ?<]
   @matching %{?{ => ?}, ?( => ?), ?[ => ?], ?< => ?>}
   @scores %{?) => 3, ?] => 57, ?} => 1197, ?> => 25137}
+  @scores2 %{?) => 1, ?] => 2, ?} => 3, ?> => 4}
 
   def analyze_line(line), do: analyze_line(line, [])
 
@@ -32,6 +33,18 @@ defmodule AdventOfCode.Day10 do
     |> sum()
   end
 
-  def part2(_args) do
+  def part2(args) do
+    scores =
+      args
+      |> String.split("\n", trim: true)
+      |> map(&to_charlist/1)
+      |> map(&analyze_line/1)
+      |> filter(&(elem(&1, 0) == :incomplete))
+      |> map(&reverse(elem(&1, 1)))
+      |> map(fn l -> map(l, fn c -> @matching[c] end) end)
+      |> map(fn line -> reduce(line, 0, fn c, acc -> acc * 5 + @scores2[c] end) end)
+      |> sort()
+
+    at(scores, div(count(scores), 2))
   end
 end
