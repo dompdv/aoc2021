3be1f4c533734fa1621ebdcf562eb96268297f3f
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2021-12-13 10:38:59 +0100

    day13 p1 and p3

--- lib/advent_of_code/day_13.ex
+++ lib/advent_of_code/day_13.ex
@@ -1,7 +1,66 @@
 defmodule AdventOfCode.Day13 do
-  def part1(_args) do
+  import Enum
+
+  @fold_axis %{"x" => :x, "y" => :y}
+  def parse(args) do
+    [points, folds] = args |> String.split("\n\n", trim: true)
+
+    points =
+      String.split(points, "\n", trim: true)
+      |> map(fn line ->
+        String.split(line, ",") |> map(&String.to_integer/1) |> List.to_tuple()
+      end)
+      |> MapSet.new()
+
+    folds =
+      folds
+      |> String.split("\n", trim: true)
+      |> map(fn line ->
+        [_, _, equation] = String.split(line, " ")
+        [axis, where] = String.split(equation, "=")
+        {@fold_axis[axis], String.to_integer(where)}
+      end)
+
+    {points, folds}
   end
 
-  def part2(_args) do
+  def fold_y(points, axis) do
+    points
+    |> map(fn {col, row} -> {col, if(row < axis, do: row, else: axis - (row - axis))} end)
+    |> MapSet.new()
+  end
+
+  def fold_x(points, axis) do
+    points
+    |> map(fn {col, row} -> {if(col < axis, do: col, else: axis - (col - axis)), row} end)
+    |> MapSet.new()
+  end
+
+  def fold(points, {:x, axis}), do: fold_x(points, axis)
+  def fold(points, {:y, axis}), do: fold_y(points, axis)
+
+  def part1(args) do
+    {points, folds} = parse(args)
+    fold(points, folds |> List.first()) |> count()
+  end
+
+  def print(points) do
+    max_row = map(points, &elem(&1, 1)) |> max()
+    max_col = map(points, &elem(&1, 0)) |> max()
+    {max_row, max_col}
+
+    for row <- 0..max_row do
+      for col <- 0..max_col do
+        if MapSet.member?(points, {col, row}), do: "#", else: " "
+      end
+    end
+    |> map(fn line -> join(line) end)
+  end
+
+  def part2(args) do
+    {points, folds} = parse(args)
+    IO.inspect("")
+    folds |> reduce(points, fn fold, p -> fold(p, fold) end) |> print() |> IO.inspect()
+    IO.inspect("")
   end
 end
--- lib/mix/tasks/d13.p1.ex
+++ lib/mix/tasks/d13.p1.ex
@@ -5,7 +5,32 @@ defmodule Mix.Tasks.D13.P1 do
 
   @shortdoc "Day 13 Part 1"
   def run(args) do
-    input = nil
+    #
+    input = AdventOfCode.Input.get!(13, 2021)
+
+    """
+    6,10
+    0,14
+    9,10
+    0,3
+    10,4
+    4,11
+    6,0
+    6,12
+    4,1
+    0,13
+    10,12
+    3,4
+    3,0
+    8,4
+    1,10
+    2,14
+    8,10
+    9,0
+
+    fold along y=7
+    fold along x=5
+    """
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_1: fn -> input |> part1() end}),
