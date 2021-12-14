2086d0beb73c6f4499055744fae18dfd69096835
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2021-12-14 09:46:00 +0100

    day14 cleaner

--- lib/advent_of_code/day_14.ex
+++ lib/advent_of_code/day_14.ex
@@ -13,49 +13,33 @@ defmodule AdventOfCode.Day14 do
 
   def apply_rules(kernel, rules) do
     kernel
-    |> chunk_every(2, 1)
-    |> map(fn doublet ->
-      case doublet do
-        [f, _] -> [f, Map.get(rules, doublet, [])] |> List.flatten()
-        [f] -> [f]
-      end
-    end)
-    |> join()
-    |> to_charlist()
-  end
-
-  def run_for(args, iterations, step) do
-    {kernel, rules} = args |> parse()
-    freqs =
-    reduce(1..iterations, kernel, fn _, acc -> step.(acc, rules) end)
-    |> frequencies()
-    |> Map.values()
-    |> sort()
-
-    at(freqs, -1) - at(freqs, 0)
-  end
-
-  def apply_rules2(kernel, rules) do
-    kernel |> map(fn { [g,d] = doublet, n} ->
+    |> map(fn {[g, d] = doublet, n} ->
       case Map.get(rules, doublet) do
         nil -> {doublet, n}
-        v -> [{[g,hd(v)], n}, {[hd(v), d], n}]
+        v -> [{[g, hd(v)], n}, {[hd(v), d], n}]
       end
-    end) |> List.flatten()
+    end)
+    |> List.flatten()
     |> reduce(%{}, fn {k, v}, acc -> Map.put(acc, k, Map.get(acc, k, 0) + v) end)
   end
-  def part1(args), do: run_for(args, 10, &apply_rules/2)
-  def part2(args) do
+
+  def run_for(args, iterations) do
     {kernel, rules} = args |> parse()
+
     freqs =
-    reduce(1..40, kernel
-    |> chunk_every(2, 1, :discard) |> frequencies(), fn _, acc -> apply_rules2(acc, rules) end)
-    |> map(fn {[g, _], v} -> {g, v} end)
-    |> reduce(%{List.last(kernel) => 1}, fn {k, v}, acc -> Map.put(acc, k, Map.get(acc, k, 0) + v) end)
-    |> Map.values()
-    |> sort()
+      reduce(1..iterations, kernel |> chunk_every(2, 1, :discard) |> frequencies(), fn _, acc ->
+        apply_rules(acc, rules)
+      end)
+      |> map(fn {[g, _], v} -> {g, v} end)
+      |> reduce(%{List.last(kernel) => 1}, fn {k, v}, acc ->
+        Map.put(acc, k, Map.get(acc, k, 0) + v)
+      end)
+      |> Map.values()
+      |> sort()
 
     at(freqs, -1) - at(freqs, 0)
   end
 
+  def part1(args), do: run_for(args, 10)
+  def part2(args), do: run_for(args, 40)
 end
