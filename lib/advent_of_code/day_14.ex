4381dc66922d4913308611644fc9c82549547307
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2021-12-14 10:06:05 +0100

    day14

--- lib/advent_of_code/day_14.ex
+++ lib/advent_of_code/day_14.ex
@@ -3,7 +3,7 @@ defmodule AdventOfCode.Day14 do
 
   def parse(args) do
     [kernel, rules] = args |> String.split("\n\n", trim: true)
-
+    # les règles sont une Map  %([car1, car2] => car_a_inserer)
     {to_charlist(kernel),
      rules
      |> String.split("\n", trim: true)
@@ -14,27 +14,34 @@ defmodule AdventOfCode.Day14 do
 
   def apply_rules(kernel, rules) do
     kernel
+    # pour chaque doublet -> frequence
     |> map(fn {[g, d] = doublet, n} ->
       case Map.get(rules, doublet) do
-        nil -> {doublet, n}
-        v -> [{[g, v], n}, {[v, d], n}]
+        nil -> {doublet, n} # pas de règle à appliquer, on ne touche à rien
+        v -> [{[g, v], n}, {[v, d], n}] # le doublet se transforme en deux autres doublets
       end
     end)
+    # suppression des listes imbriquées
     |> List.flatten()
+    # consolidation des fréquences pour les doublets identiques
     |> reduce(%{}, fn {k, v}, acc -> Map.put(acc, k, Map.get(acc, k, 0) + v) end)
   end
 
   def run_for(args, iterations) do
     {kernel, rules} = args |> parse()
 
+    # Mise à jour itérative du tableau des fréquence de doublets de lettres. %{ 'NN' => 12, 'BC" => 1}
     freqs =
       reduce(1..iterations, kernel |> chunk_every(2, 1, :discard) |> frequencies(), fn _, acc ->
         apply_rules(acc, rules)
       end)
+      # On ne garde que la première lettre de chaque doublet (la deuxième est en fait la première d'un autre doublet)
       |> map(fn {[g, _], v} -> {g, v} end)
+      # On ajoute les frequences des lettres identiques. Attention, il faut ajouter la dernière lettre du noyau initial
       |> reduce(%{List.last(kernel) => 1}, fn {k, v}, acc ->
         Map.put(acc, k, Map.get(acc, k, 0) + v)
       end)
+      # tri des fréquences
       |> Map.values()
       |> sort()
 
