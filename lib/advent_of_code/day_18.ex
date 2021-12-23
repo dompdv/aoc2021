ba2700c7f61156c2a573a1c8371a47996d472133
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2021-12-23 14:51:35 +0100

    d18 explode

--- lib/advent_of_code/day_18.ex
+++ lib/advent_of_code/day_18.ex
@@ -17,7 +17,6 @@ defmodule AdventOfCode.Day18 do
   def element([e | r]), do: {e, r}
 
   def pair([:ob, :ob | r]) do
-    #IO.inspect({"obob", r})
     {left_b, [:comma | rest_l]} = pair([:ob | r])
     {right_b, rest_d} = element(rest_l)
     [:cb | final_rest] = rest_d
@@ -25,7 +24,6 @@ defmodule AdventOfCode.Day18 do
   end
 
   def pair([:ob, e | r]) do
-    #IO.inspect({"ob",e, r})
     left_b = e
     [:comma | rest_l] = r
     {right_b, rest_d} = element(rest_l)
@@ -41,9 +39,11 @@ defmodule AdventOfCode.Day18 do
   def detect_explode(tree), do: detect_explode(tree, [])
   def detect_explode({:num, _, _}, _), do: false
   def detect_explode({:pair, {:num, le, _}, {:num, ri, _}, 4}, l), do: {true, reverse(l), le, ri}
+
   def detect_explode({:pair, le, ri, _}, l) do
     compute_left = detect_explode(le, [:left | l])
     compute_right = detect_explode(ri, [:right | l])
+
     cond do
       compute_left != false -> compute_left
       compute_right != false -> compute_right
@@ -55,26 +55,34 @@ defmodule AdventOfCode.Day18 do
   def nullify({:pair, le, ri, level}, [:left | l]), do: {:pair, nullify(le, l), ri, level}
   def nullify({:pair, le, ri, level}, [:right | l]), do: {:pair, le, nullify(ri, l), level}
 
+  def list_numbers(tree), do: list_numbers([], tree, []) |> reverse()
+  def list_numbers(paths, {:num, _, _}, l), do: [reverse(l) | paths]
 
-  def add_to_right({:pair, le, ri, level}, value,  [:left | l]), do: {:pair, add_to_right(le, value, l), ri, level}
-  def add_to_right({:pair, le, ri, level}, value,  [:right | l]), do: {:pair, le, add_to_right(ri, value, l), level}
-  def add_to_right({:num, e, level}, value,[]), do: {:num, e + value, level}
-  def add_to_right({:pair, le, ri, level}, value, []) do
-    tree_right = add_to_right(ri, value, [])
-    if tree_right != false do
-      {:pair, le, tree_right, level}
-    else
-      tree_left = add_to_right(le, value, [])
-      if tree_left != false, do: {:pair, tree_left, ri, level}, else: false
-    end
+  def list_numbers(paths, {:pair, le, ri, _}, l) do
+    paths |> list_numbers(le, [:left | l]) |> list_numbers(ri, [:right | l])
+  end
+
+  def tree_apply({:num, val, level}, [], f), do: {:num, f.(val), level}
+
+  def tree_apply({:pair, le, ri, level}, [:left | l], f),
+    do: {:pair, tree_apply(le, l, f), ri, level}
+
+  def tree_apply({:pair, le, ri, level}, [:right | l], f),
+    do: {:pair, le, tree_apply(ri, l, f), level}
+
+  def print_tree({:num, val, _level}), do: Integer.to_string(val)
+
+  def print_tree({:pair, le, ri, _level}) do
+    left = print_tree(le)
+    right = print_tree(ri)
+    "[#{left},#{right}]"
   end
 
   def part1(_args) do
     tree =
-      #"[[[[1,2],[3,4]],[[5,6],[7,8]]],9]"
-
-      #"[[6,[5,[4,[3,2]]]],1]"
-      "[[[[[9,8],1],2],3],4]"
+      "[7,[6,[5,[4,[3,2]]]]]"
+      # "[[[[[9,8],1],2],3],4]"
+      # "[[6,[5,[4,[3,2]]]],1]"
       |> to_charlist()
       |> tokenize()
       |> pair()
@@ -82,13 +90,28 @@ defmodule AdventOfCode.Day18 do
       |> add_levels(0)
       |> IO.inspect()
 
+    IO.inspect("liste 1")
+    list_numbers(tree) |> IO.inspect()
+    {true, path, le_value, ri_value} = detect_explode(tree)
 
-      {true, path, le, ri} = detect_explode(tree)
-      IO.inspect({"explode", path})
-      tree = nullify(tree, path) |> IO.inspect()
-      IO.inspect({"add to right"})
-      s_path = (drop(path, -1) ++ [:left]) |> IO.inspect()
-      add_to_right(tree, le, s_path)
+    IO.inspect({"explode", path})
+    tree = nullify(tree, path) |> IO.inspect()
+
+    paths_for_numbers = list_numbers(tree) |> IO.inspect()
+
+    index = find_index(paths_for_numbers, fn x -> x >= path end)
+
+    tree =
+      if index <= 0,
+        do: tree,
+        else: tree_apply(tree, at(paths_for_numbers, index - 1), fn x -> x + le_value end)
+
+    tree =
+      if index + 1 >= count(paths_for_numbers),
+        do: tree,
+        else: tree_apply(tree, at(paths_for_numbers, index + 1), fn x -> x + ri_value end)
+
+    tree |> print_tree()
   end
 
   def part2(_args) do
