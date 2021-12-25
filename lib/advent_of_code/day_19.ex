1e6a2415b554909beaa921a194ae96c2fadd81cc
dominique <dompdv@yahoo.fr>
2021-12-25 16:23:37 +0100

    d19 sauv avant autre essai

--- lib/advent_of_code/day_19.ex
+++ lib/advent_of_code/day_19.ex
@@ -1,9 +1,18 @@
 defmodule AdventOfCode.Day19 do
   import Enum
 
-  @flip [[0,1,2], [2,0,1], [1,2,0]]
-  @inversions [[1,1,1], [1,1,-1], [1,-1,1], [1,-1,-1], [-1,1,1], [-1,1,-1], [-1,-1,1], [-1,-1,-1]]
-  @transfos for f <- @flip, i <- @inversions, do: {f,i}
+  @flip [[0, 1, 2], [2, 0, 1], [1, 2, 0]]
+  @inversions [
+    [1, 1, 1],
+    [1, 1, -1],
+    [1, -1, 1],
+    [1, -1, -1],
+    [-1, 1, 1],
+    [-1, 1, -1],
+    [-1, -1, 1],
+    [-1, -1, -1]
+  ]
+  @transfos for f <- @flip, i <- @inversions, do: {f, i}
 
   def parse_scanner(scanner) do
     [head | lines] = String.split(scanner, "\n", trim: true)
@@ -33,13 +42,22 @@ defmodule AdventOfCode.Day19 do
   def dist_scanners(scanner1, scanner2, p) do
     scanner2 = move_scanner(scanner2, p)
 
-    for(
-      {x1, y1, z1} <- scanner1,
-      {x2, y2, z2} <- scanner2,
-      do: (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2)
-    )
-    |> sum()
-    |> :math.sqrt()
+    distances =
+      for(
+        {x2, y2, z2} <- scanner2,
+        do:
+          for(
+            {x1, y1, z1} <- scanner1,
+            do: (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2)
+          )
+          |> min()
+          |> :math.sqrt()
+      )
+
+    nuls = filter(distances, &(&1 == 0)) |> count()
+
+    # IO.inspect({"nulllllls", nuls})
+    sum(distances)
   end
 
   def gradient(scanner1, scanner2, {x, y, z}) do
@@ -53,28 +71,40 @@ defmodule AdventOfCode.Day19 do
   def move({x, y, z}, {gx, gy, gz}, alpha),
     do: {round(x + alpha * gx), round(y + alpha * gy), round(z + alpha * gz)}
 
-  def apply_transfo(scanner, {[f0,f1,f2],[i0,i1,i2]}) do
-    scanner |> map(fn p ->
+  def apply_transfo(scanner, {[f0, f1, f2], [i0, i1, i2]}) do
+    scanner
+    |> map(fn p ->
       {elem(p, f0) * i0, elem(p, f1) * i1, elem(p, f2) * i2}
     end)
   end
 
   def part1(args) do
     scanners = args |> String.split("\n\n", trim: true) |> map(&parse_scanner/1) |> Map.new()
-    alpha = 3
+    alpha = 0.5
 
+    Map.put(scanners, 1, move_scanner(scanners[0], {50, 20, 30}))
 
-    @transfos |> map(fn transfo ->
+    a = [
+      @transfos
+      |> List.first()
+    ]
+
+    a
+    |> map(fn transfo ->
       scanner1 = apply_transfo(scanners[1], transfo)
+
       res =
-      Stream.iterate({{0, 0, 0}, nil, nil, 0}, fn {{x, y, z}, _g, _d, step} ->
-        {{gx, gy, gz}, d} = gradient(scanners[0], scanner1, {x, y, z})
-        {move({x, y, z}, {gx, gy, gz}, alpha), {gx, gy, gz}, d, step + 1}
-      end)
-      |> Stream.drop_while(fn {_, _, _, step} -> step < 1000 end)
-      |> take(1)
+        Stream.iterate({{0, 0, 0}, nil, nil, 0}, fn {{x, y, z}, _g, _d, step} = r ->
+          IO.inspect({">", r})
+          {{gx, gy, gz}, d} = gradient(scanners[0], scanner1, {x, y, z})
+          {move({x, y, z}, {gx, gy, gz}, alpha), {gx, gy, gz}, d, step + 1}
+        end)
+        |> Stream.drop_while(fn {_, _, _, step} -> step < 100 end)
+        |> take(1)
+
       IO.inspect({transfo, res})
     end)
+
     :ok
   end
 
