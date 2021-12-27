73a53f8ee55153ada19cf68a576a83bdc028a437
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2021-12-27 10:29:25 +0100

    d20 p1

--- lib/advent_of_code/day_20.ex
+++ lib/advent_of_code/day_20.ex
@@ -17,16 +17,16 @@ defmodule AdventOfCode.Day20 do
     {algo, {image, count(image), count(at(image, 0))}}
   end
 
-  def img_get({_img, rows, cols}, row, col)
+  def img_get({_img, rows, cols}, row, col, step, algo)
       when row < 0 or col < 0 or row >= rows or col >= cols,
-      do: 0
+      do: if rem(step, 2) == 0, do: at(algo, 511), else: at(algo, 0)
 
-  def img_get({img, _rows, _cols}, row, col), do: img |> at(row) |> at(col)
+  def img_get({img, _rows, _cols}, row, col, _step, _algo), do: img |> at(row) |> at(col)
 
-  def convol(row, col, algo, image) do
+  def convol(row, col, algo, image, step) do
     index =
       @filter
-      |> map(fn {dr, dc} -> img_get(image, row + dr, col + dc) end)
+      |> map(fn {dr, dc} -> img_get(image, row + dr, col + dc, step, algo) end)
       |> reverse()
       |> reduce({1, 0}, fn n, {exp, acc} -> {exp * 2, acc + n * exp} end)
       |> elem(1)
@@ -34,15 +34,23 @@ defmodule AdventOfCode.Day20 do
     at(algo, index)
   end
 
-  def one_step({algo, {_img, rows, cols} = image}) do
-    image = for row <- -1..rows, do: for(col <- -1..cols, do: convol(row, col, algo, image))
+  def one_step({algo, {_img, rows, cols} = image}, step) do
+    image = for row <- -1..rows, do: for(col <- -1..cols, do: convol(row, col, algo, image, step))
     {algo, {image, count(image), count(at(image, 0))}}
   end
 
-  def part1(args) do
-    parse(args) |> one_step() |> one_step() |> elem(1) |> elem(0) |> List.flatten() |> sum()
+  def print_image({img, _rows, _cols}) do
+    img |> map(fn line ->
+      map(line, fn c -> if c == 0, do: '.', else: '#' end) |> to_string()
+    end)
   end
 
-  def part2(_args) do
+  def launch(args, n) do
+    {_, {img, _, _}} =
+     reduce(1..n, parse(args), fn step, acc -> one_step(acc, step) end)
+    img |> List.flatten() |> sum()
   end
+
+  def part1(args), do: launch(args, 2)
+  def part2(args), do: launch(args, 50)
 end
