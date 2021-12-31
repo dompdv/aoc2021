edcb800c9f863cc1162e0a7789cbda38857dd180
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2021-12-31 18:41:08 +0100

    bof

--- lib/advent_of_code/day_21.ex
+++ lib/advent_of_code/day_21.ex
@@ -1,7 +1,25 @@
+defmodule MemoDay21 do
+  use Agent
+
+  def start_link() do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
+
+  def get(key) do
+    Agent.get(__MODULE__, fn state -> Map.get(state, key) end)
+  end
+
+  def set(key, value) do
+    Agent.update(__MODULE__, fn state -> Map.put_new_lazy(state, key, fn -> value end) end)
+  end
+end
+
 defmodule AdventOfCode.Day21 do
   import Enum
 
-  @outcomes for(i <- 1..3, j <- 1..3, k <- 1..3, do: [i, j, k]) |> map(&sum/1) |> frequencies()
+  @outcomes for(i <- 1..3, j <- 1..3, k <- 1..3, do: [i, j, k]) |> map(&sum/1)
+  @cross_outcomes (for a <- @outcomes, b <- @outcomes, do: {a,b}) |> frequencies()
+
   def move_by(shift, {pos, score}) do
     arrival = rem(pos - 1 + shift, 10) + 1
     {arrival, score + arrival}
@@ -29,76 +47,35 @@ defmodule AdventOfCode.Day21 do
     n_rolls * min([p1, p2])
   end
 
-  def compute_outcomes(pos, score, n_events, board, wins_so_far) do
-    # boucle sur chaque tirage possible. @outcome est %{tirage => nombres d'occurences}
+  def init_cache() do
 
-    reduce(
-      @outcomes,
-      {board, wins_so_far},
-     fn {rolls, outcomes}, {acc_board, acc_wins_so_far} ->
-#        fn rolls, {acc_board, acc_wins_so_far} ->
-#    outcomes = 1
-        # etant donné un jet de dés (rolls) et un nombre d'outcomes pour ce jet de dés
-        # on calcule où on atterit (nouvelle position, nouveau score)
-        {new_pos, new_score} = move_by(rolls, {pos, score})
-
-        if new_score >= 21 do
-          # si on a gagnés, on incrémente le compteur des victoires
-          {acc_board, acc_wins_so_far + n_events * outcomes}
-        else
-          # si on n'a pas gagné, on va mettre à jour, dans le nouveau board, le nombre d'événements y conduisant
-          previous_n_events = Map.get(acc_board, {new_pos, new_score}, 0)
-
-          {Map.put(
-             acc_board,
-             {new_pos, new_score},
-             # nombre d'événements de départ * le nombre d'outcomes du jet de dés
-             previous_n_events + n_events * outcomes
-           ), acc_wins_so_far}
-        end
-      end
-    )
   end
 
-  def one_turn(event_board) do
-    # Le board est un tableau d'états {position, score} => nombre d'événements aboutissant à cet état
-    # renvoie un couple {new board, wins} "wins" est le nombre d'événements arrivant à 21 et plus
+  def wins(_pos1, score1, _pos2, _score2) when score1 >= 21, do: 1
+  def wins(_pos1, _score1, _pos2, score2) when score2 >= 21, do: 0
 
-    # Boucle sur toutes les cases du du board.
-    # on fabrique le board du tour suivant (accumulation des wins)
-    reduce(event_board, {%{}, 0}, fn {{pos, score}, n_events}, {board, wins_so_far} ->
-      compute_outcomes(pos, score, n_events, board, wins_so_far)
-    end)
-  end
-
-  def move(board1, board2, win1, win2) do
-    #print_board(board1)
-    if empty?(board1) or empty?(board2) do
-      {win1, win2}
+  def wins(pos1, score1, pos2, score2) do
+    memo = MemoDay21.get({pos1, score1, pos2, score2})
+    if memo != nil do
+      memo
     else
-      {board1, new_win1} = one_turn(board1)
-      {board2, new_win2} = one_turn(board2)
-      move(board1, board2, win1 + new_win1, win2 + new_win2)
+      @cross_outcomes |> map(fn {{rolls1, rolls2}, occurences} ->
+
+        {new_pos1, new_score1} = move_by(rolls1, {pos1, score1})
+        total_wins1 = occurences * wins(new_pos1, new_score1, pos2, score2)
+        MemoDay21.set({new_pos1, new_score1, pos2, score2}, total_wins1)
+
+        {new_pos2, new_score2} = move_by(rolls2, {pos2, score2})
+        total_wins2 = total_wins1 + occurences * wins(new_pos1, new_score1, new_pos2, new_score2)
+        MemoDay21.set({new_pos1, new_score1, new_pos2, new_score2}, total_wins2)
+        total_wins2
+      end) |> sum()
+
     end
   end
-
-  def print_board(board) do
-    m = if empty?(board), do: 0, else:
-    board |> Map.values() |> max() |> Integer.to_string() |> String.length()
-    ["\n"] ++
-    (for pos <- 1..10,
-        do:
-          for(
-            score <- 0..20,
-            do:
-              Map.get(board, {pos, score}, 0) |> Integer.to_string() |> String.pad_leading(m + 1)
-          ) |> join())
-    |> join("\n")
-    |> IO.puts()
-  end
-
   def part2(_args) do
-    # {a,_} = {%{{1, 0} => 1}, 0} |> one_turn() |> IO.inspect() |> one_turn()
-    move(%{{4, 0} => 1}, %{{8, 0} => 1}, 0, 0)
+    #@cross_outcomes |> frequencies()
+    MemoDay21.start_link()
+    wins(4,0,8,0)
   end
 end
