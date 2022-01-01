c50901b2fe95e48a6394d7946f872f906aee56f6
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2022-01-01 18:36:40 +0100

    d22 p2 qui marche !!

--- lib/advent_of_code/day_21.ex
+++ lib/advent_of_code/day_21.ex
@@ -3,7 +3,7 @@ defmodule AdventOfCode.Day21 do
 
   # |> frequencies()
   @outcomes for(i <- 1..3, j <- 1..3, k <- 1..3, do: [i, j, k]) |> map(&sum/1)
-  @outcomes_cross for(i <- @outcomes, j <- @outcomes, do: {i, j}) |> frequencies()
+  @outcomes_freq @outcomes |> frequencies()
   def move_by(shift, {pos, score}) do
     arrival = rem(pos - 1 + shift, 10) + 1
     {arrival, score + arrival}
@@ -33,28 +33,35 @@ defmodule AdventOfCode.Day21 do
 
   def compute_outcomes(pos1, score1, pos2, score2, n_events, board, wins_so_far1, wins_so_far2) do
     reduce(
-      @outcomes_cross,
+      @outcomes_freq,
       {board, wins_so_far1, wins_so_far2},
-      fn {{rolls1, rolls2}, occurences}, {acc_board, acc_wins_so_far1, acc_wins_so_far2} ->
+      fn {rolls1, occurences1}, {acc_board, acc_wins_so_far1, acc_wins_so_far2} ->
         {new_pos1, new_score1} = move_by(rolls1, {pos1, score1})
-        {new_pos2, new_score2} = move_by(rolls2, {pos2, score2})
-        new_events = n_events * occurences
 
         if new_score1 >= 21 do
-          {acc_board, acc_wins_so_far1 + new_events, acc_wins_so_far2}
+          {acc_board, acc_wins_so_far1 + n_events * occurences1, acc_wins_so_far2}
         else
-          if new_score2 >= 21 do
-            {acc_board, acc_wins_so_far1, acc_wins_so_far2 + new_events}
-          else
-            previous_n_events =
-              Map.get(acc_board, {new_pos1, new_score1, new_pos2, new_score2}, 0)
+          reduce(
+            @outcomes_freq,
+            {acc_board, acc_wins_so_far1, acc_wins_so_far2},
+            fn {rolls2, occurences2}, {acc_board, acc_wins_so_far1, acc_wins_so_far2} ->
+              {new_pos2, new_score2} = move_by(rolls2, {pos2, score2})
+              new_events = n_events * occurences1 * occurences2
 
-            {Map.put(
-               acc_board,
-               {new_pos1, new_score1, new_pos2, new_score2},
-               previous_n_events + new_events
-             ), acc_wins_so_far1, acc_wins_so_far2}
-          end
+              if new_score2 >= 21 do
+                {acc_board, acc_wins_so_far1, new_events + acc_wins_so_far2}
+              else
+                previous_n_events =
+                  Map.get(acc_board, {new_pos1, new_score1, new_pos2, new_score2}, 0)
+
+                {Map.put(
+                   acc_board,
+                   {new_pos1, new_score1, new_pos2, new_score2},
+                   previous_n_events + new_events
+                 ), acc_wins_so_far1, acc_wins_so_far2}
+              end
+            end
+          )
         end
       end
     )
@@ -62,7 +69,7 @@ defmodule AdventOfCode.Day21 do
 
   def one_turn(event_board, win1, win2) do
     # Le board est un tableau d'états {position1, score1, position2, score2} => nombre d'événements aboutissant à cet état
-    # renvoie un couple {new board, wins} "wins" est le nombre d'événements arrivant à 21 et plus
+    # renvoie {new board, wins_player1, wins_player2} "wins" est le nombre d'événements arrivant à 21 et plus
     reduce(
       event_board,
       {%{}, win1, win2},
@@ -76,13 +83,13 @@ defmodule AdventOfCode.Day21 do
     if empty?(board) do
       {win1, win2}
     else
-      {board, win1p, win2p} = one_turn(board, win1, win2)
-      IO.inspect({win1p, win2p})
-      move(board, win1p, win2p, turn + 1)
+      {new_board, new_win1, new_win2} = one_turn(board, win1, win2)
+      IO.inspect({new_win1, new_win2})
+      move(new_board, new_win1, new_win2, turn + 1)
     end
   end
 
   def part2(_args) do
-    move(%{{4, 0, 8, 0} => 1}, 0, 0, 0)
+    move(%{{7, 0, 6, 0} => 1}, 0, 0, 0)
   end
 end
