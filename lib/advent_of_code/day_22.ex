3c1ca765ffe7abbae03e8cb0e2dbd9f7b6a65e2a
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2022-01-03 17:28:33 +0100

    d22 attempt 01

--- lib/advent_of_code/day_22.ex
+++ lib/advent_of_code/day_22.ex
@@ -32,10 +32,14 @@ defmodule AdventOfCode.Day22 do
     xmax = Kernel.min(@dim_max, xmax)
     ymax = Kernel.min(@dim_max, ymax)
     zmax = Kernel.min(@dim_max, zmax)
+
     if switch == :off do
-      cuboid |> Map.drop(for x <- xmin..xmax, y <- ymin..ymax, z <- zmin..zmax, do: {x,y,z})
+      cuboid |> Map.drop(for x <- xmin..xmax, y <- ymin..ymax, z <- zmin..zmax, do: {x, y, z})
     else
-      cuboid |> Map.merge((for x <- xmin..xmax, y <- ymin..ymax, z <- zmin..zmax, into: %{}, do: {{x,y,z},1}))
+      cuboid
+      |> Map.merge(
+        for x <- xmin..xmax, y <- ymin..ymax, z <- zmin..zmax, into: %{}, do: {{x, y, z}, 1}
+      )
     end
   end
 
@@ -48,6 +52,23 @@ defmodule AdventOfCode.Day22 do
     |> count()
   end
 
+  def merge_intervals([a]=l), do: [a,a]
+  def merge_intervals([{_b1, h1}, {b2, _h2}]=l) when h1 < b2, do: l
+  def merge_intervals([{b1, h1}, {_b2, h2}]), do: [{b1, max([h1, h2])}, :merge]
+
+
+  def clean_intervals(intervals), do: clean_intervals(intervals, [], false)
+  def clean_intervals([{b1, h1}, {b2, h2}|r], acc, merged) when (h1 + 1) < b2, do: clean_intervals([{b2,h2}|r], [{b1,h1}|acc], merged)
+  def clean_intervals([{b1, h1}, {b2, h2}|r], acc, _merged) when b2 <= (h1 + 1), do: clean_intervals(r, [{b1, max([h1, h2])}|acc], true)
+  def clean_intervals([i], acc, merged) do
+    if merged, do: clean_intervals(reverse([i|acc]), [], false), else: reverse([i|acc])
+  end
+
+  def add_interval(l, interval), do: sort([interval | l]) |> clean_intervals()
+
   def part2(_args) do
+    [] |> add_interval({10, 15}) |> add_interval({20, 25}) |> add_interval({5, 13}) |> add_interval({8, 18}) |> add_interval({30, 32})
+    |> IO.inspect()
+
   end
 end
