65d594d5940482ad061082d0a9a5d63b890744e8
dominique <dompdv@yahoo.fr>
2022-01-16 17:48:53 +0100

    d23 tem

--- lib/advent_of_code/day_16.ex
+++ lib/advent_of_code/day_16.ex
@@ -34,46 +34,83 @@ defmodule AdventOfCode.Day16 do
 
   def packet([]), do: nil
 
-  def packet([a, b, c, 1, 0, 0| r]) do
+  def packet([a, b, c, 1, 0, 0 | r]) do
     {val, new_r} = packet_value(r)
-    {%{type: :literal, tn: 4, version: to_number([a,b,c]), value: to_number(val)}, new_r}
+    {%{type: :literal, tn: 4, version: to_number([a, b, c]), value: to_number(val)}, new_r}
   end
 
-  def packet([a, b, c, ta, tb, tc, 0, l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11,l12,l13,l14,l15 | r]) do
-    len = to_number([l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11,l12,l13,l14,l15])
+  def packet([
+        a,
+        b,
+        c,
+        ta,
+        tb,
+        tc,
+        0,
+        l1,
+        l2,
+        l3,
+        l4,
+        l5,
+        l6,
+        l7,
+        l8,
+        l9,
+        l10,
+        l11,
+        l12,
+        l13,
+        l14,
+        l15 | r
+      ]) do
+    len = to_number([l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13, l14, l15])
     target = count(r) - len
-    {new_r, packets} =
-    Stream.iterate(0, &(&1+1))
-    |> reduce_while({r, []}, fn _n, {rest, acc} ->
-      if count(rest) <= target do
-        {:halt, {rest, acc}}
-      else
-        {new_packet, new_rest} = packet(rest)
-        {:cont, {new_rest, [new_packet|acc]}}
-      end
-    end)
 
-    {%{type: :operator, tn: to_number([ta,tb,tc]), version: to_number([a,b,c]), packets: reverse(packets)}, new_r}
+    {new_r, packets} =
+      Stream.iterate(0, &(&1 + 1))
+      |> reduce_while({r, []}, fn _n, {rest, acc} ->
+        if count(rest) <= target do
+          {:halt, {rest, acc}}
+        else
+          {new_packet, new_rest} = packet(rest)
+          {:cont, {new_rest, [new_packet | acc]}}
+        end
+      end)
+
+    {%{
+       type: :operator,
+       tn: to_number([ta, tb, tc]),
+       version: to_number([a, b, c]),
+       packets: reverse(packets)
+     }, new_r}
   end
 
-  def packet([a, b, c, ta, tb, tc, 1, l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11 | r]) do
-    len = to_number([l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11])
-    {new_r, packets} =
-    Stream.iterate(0, &(&1+1))
-    |> reduce_while({r, []}, fn _n, {rest, acc} ->
-      if count(acc) >= len do
-        {:halt, {rest, acc}}
-      else
-        {new_packet, new_rest} = packet(rest)
-        {:cont, {new_rest, [new_packet|acc]}}
-      end
-    end)
+  def packet([a, b, c, ta, tb, tc, 1, l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11 | r]) do
+    len = to_number([l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11])
 
-    {%{type: :operator, tn: to_number([ta,tb,tc]), version: to_number([a,b,c]), packets: reverse(packets)}, new_r}
+    {new_r, packets} =
+      Stream.iterate(0, &(&1 + 1))
+      |> reduce_while({r, []}, fn _n, {rest, acc} ->
+        if count(acc) >= len do
+          {:halt, {rest, acc}}
+        else
+          {new_packet, new_rest} = packet(rest)
+          {:cont, {new_rest, [new_packet | acc]}}
+        end
+      end)
+
+    {%{
+       type: :operator,
+       tn: to_number([ta, tb, tc]),
+       version: to_number([a, b, c]),
+       packets: reverse(packets)
+     }, new_r}
   end
 
   def add_version_number(%{type: :literal, version: v}), do: v
-  def add_version_number(%{type: :operator, version: v, packets: packets}), do: v + sum(map(packets, &add_version_number/1))
+
+  def add_version_number(%{type: :operator, version: v, packets: packets}),
+    do: v + sum(map(packets, &add_version_number/1))
 
   def product(l), do: reduce(l, 1, &(&1 * &2))
   def compute(%{type: :literal, value: val}), do: val
@@ -81,15 +118,17 @@ defmodule AdventOfCode.Day16 do
   def compute(%{tn: 1, packets: packets}), do: packets |> map(&compute/1) |> product()
   def compute(%{tn: 2, packets: packets}), do: packets |> map(&compute/1) |> min()
   def compute(%{tn: 3, packets: packets}), do: packets |> map(&compute/1) |> max()
-  def compute(%{tn: 5, packets: packets}), do: (if compute(at(packets,0)) > compute(at(packets,1)), do: 1, else: 0)
-  def compute(%{tn: 6, packets: packets}), do: (if compute(at(packets,0)) < compute(at(packets,1)), do: 1, else: 0)
-  def compute(%{tn: 7, packets: packets}), do: (if compute(at(packets,0)) == compute(at(packets,1)), do: 1, else: 0)
 
-  def part1(args) do
-    parse(args) |> packet() |> elem(0) |> add_version_number()
-  end
+  def compute(%{tn: 5, packets: packets}),
+    do: if(compute(at(packets, 0)) > compute(at(packets, 1)), do: 1, else: 0)
 
-  def part2(args) do
-    parse(args) |> packet() |> elem(0) |> compute()
-  end
+  def compute(%{tn: 6, packets: packets}),
+    do: if(compute(at(packets, 0)) < compute(at(packets, 1)), do: 1, else: 0)
+
+  def compute(%{tn: 7, packets: packets}),
+    do: if(compute(at(packets, 0)) == compute(at(packets, 1)), do: 1, else: 0)
+
+  def part1(args), do: parse(args) |> packet() |> elem(0) |> add_version_number()
+
+  def part2(args), do: parse(args) |> packet() |> elem(0) |> compute()
 end
--- lib/advent_of_code/day_23.ex
+++ lib/advent_of_code/day_23.ex
@@ -1,5 +1,150 @@
 defmodule AdventOfCode.Day23 do
+  import Enum
+  @infinite 999_999_999_999
+
+  @maze %{
+    0 => [1],
+    1 => [0, 2],
+    2 => [1, 3, 11],
+    3 => [2, 4],
+    4 => [3, 5, 13],
+    5 => [4, 6],
+    6 => [5, 7, 15],
+    7 => [6, 8],
+    8 => [7, 9, 17],
+    9 => [8, 10],
+    10 => [9],
+    11 => [2, 12],
+    12 => [11],
+    13 => [4, 14],
+    14 => [13],
+    15 => [6, 16],
+    16 => [15],
+    17 => [8, 18],
+    18 => [17]
+  }
+
+  @forbidden_moves MapSet.new([
+                     {1, 4, 13},
+                     {5, 4, 13},
+                     {1, 6, 15},
+                     {5, 6, 15},
+                     {1, 8, 17},
+                     {5, 8, 17},
+                     {2, 2, 11},
+                     {6, 2, 11},
+                     {2, 6, 15},
+                     {6, 6, 15},
+                     {2, 8, 17},
+                     {6, 8, 17},
+                     {3, 4, 13},
+                     {7, 4, 13},
+                     {3, 2, 11},
+                     {7, 2, 11},
+                     {3, 8, 17},
+                     {7, 8, 17},
+                     {4, 4, 13},
+                     {8, 4, 13},
+                     {4, 6, 15},
+                     {8, 6, 15},
+                     {4, 2, 11},
+                     {8, 2, 11}
+                   ])
+  def print_cell(i, [a1, a2, b1, b2, c1, c2, d1, d2]) do
+    cond do
+      a1 == i or a2 == i -> ?A
+      b1 == i or b2 == i -> ?B
+      c1 == i or c2 == i -> ?C
+      d1 == i or d2 == i -> ?D
+      true -> ?.
+    end
+  end
+
+  def print(state) do
+    l0 = for _ <- 1..13, do: ?#
+    l1 = [?#] ++ for(i <- 0..10, do: print_cell(i, state)) ++ [?#]
+
+    l2 = [
+      32,
+      32,
+      ?#,
+      print_cell(11, state),
+      ?#,
+      print_cell(13, state),
+      ?#,
+      print_cell(15, state),
+      ?#,
+      print_cell(17, state),
+      ?#,
+      32,
+      32,
+      32
+    ]
+
+    l3 = [
+      32,
+      32,
+      ?#,
+      print_cell(12, state),
+      ?#,
+      print_cell(14, state),
+      ?#,
+      print_cell(16, state),
+      ?#,
+      print_cell(18, state),
+      ?#,
+      32,
+      32,
+      32
+    ]
+
+    l4 = [32, 32, ?#, ?#, ?#, ?#, ?#, ?#, ?#, ?#, ?#, 32, 32]
+    [l0, l1, l2, l3, l4] |> join("\n") |> IO.puts()
+  end
+
+  def possible_move({positions, {who, _from, to}, _best_score, _moves, _win}) do
+    occupied_cells = MapSet.new(positions)
+    open_corridor_a = not MapSet.member?(occupied_cells, 11)
+    open_corridor_b = not MapSet.member?(occupied_cells, 13)
+    open_corridor_c = not MapSet.member?(occupied_cells, 15)
+    open_corridor_d = not MapSet.member?(occupied_cells, 17)
+
+    cond do
+      # Pods able to enter their corridor => priority
+      to == 2 and (who == 1 or who == 5) and open_corridor_a ->
+        [{who, to, 11}]
+
+      to == 4 and (who == 2 or who == 6) and open_corridor_b ->
+        [{who, to, 13}]
+
+      to == 6 and (who == 3 or who == 7) and open_corridor_c ->
+        [{who, to, 15}]
+
+      to == 8 and (who == 4 or who == 8) and open_corridor_d ->
+        [{who, to, 17}]
+
+      # pods cant stay
+      to == 2 or to == 4 or to == 6 or to == 8 ->
+        moves =
+          [{who, to, to - 1}, {who, to, to + 1}]
+          |> filter(fn {_, _, dest} -> not MapSet.member?(occupied_cells, dest) end)
+
+        if empty?(moves), do: :invalid, else: moves
+
+      true ->
+        for {pos, pod} <- with_index(positions),
+            do:
+              for(dest <- @maze[pos], do: {pod, pos, dest})
+              |> List.flatten()
+              |> filter(fn {_, _, dest} -> not MapSet.member?(occupied_cells, dest) end)
+              |> filter(fn move -> not MapSet.member?(@forbidden_moves, move) end)
+    end
+  end
+
   def part1(_args) do
+    start = [12, 18, 11, 15, 13, 16, 14, 17]
+    print(start)
+    possible_move({start, {nil, -1, -1}, @infinite, [], false})
   end
 
   def part2(_args) do
