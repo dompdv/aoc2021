63fd941ff98d8bd6fd0388a592a9a6102b08d265
dominique <dompdv@yahoo.fr>
2022-02-06 10:17:51 +0100

    transfer

--- lib/advent_of_code/day_23.ex
+++ lib/advent_of_code/day_23.ex
@@ -2,92 +2,6 @@ defmodule AdventOfCode.Day23 do
   import Enum
   @infinite 999_999_999_999
 
-  @maze %{
-    0 => %{
-      0 => [1],
-      1 => [0, 2],
-      2 => [1, 3, 12],
-      3 => [2, 4],
-      4 => [3, 5],
-      5 => [4, 6],
-      6 => [5, 7],
-      7 => [6, 8],
-      8 => [7, 9],
-      9 => [8, 10],
-      10 => [9],
-      12 => [2, 13],
-      13 => [],
-      14 => [4, 15],
-      15 => [14],
-      16 => [6, 17],
-      17 => [16],
-      18 => [8, 19],
-      19 => [18]
-    },
-    1 => %{
-      0 => [1],
-      1 => [0, 2],
-      2 => [1, 3],
-      3 => [2, 4],
-      4 => [3, 5, 14],
-      5 => [4, 6],
-      6 => [5, 7],
-      7 => [6, 8],
-      8 => [7, 9],
-      9 => [8, 10],
-      10 => [9],
-      12 => [2, 13],
-      13 => [12],
-      14 => [4, 15],
-      15 => [],
-      16 => [6, 17],
-      17 => [16],
-      18 => [8, 19],
-      19 => [18]
-    },
-    2 => %{
-      0 => [1],
-      1 => [0, 2],
-      2 => [1, 3],
-      3 => [2, 4],
-      4 => [3, 5],
-      5 => [4, 6],
-      6 => [5, 7, 16],
-      7 => [6, 8],
-      8 => [7, 9],
-      9 => [8, 10],
-      10 => [9],
-      12 => [2, 13],
-      13 => [12],
-      14 => [4, 15],
-      15 => [14],
-      16 => [6, 17],
-      17 => [],
-      18 => [8, 19],
-      19 => [18]
-    },
-    3 => %{
-      0 => [1],
-      1 => [0, 2],
-      2 => [1, 3],
-      3 => [2, 4],
-      4 => [3, 5],
-      5 => [4, 6],
-      6 => [5, 7],
-      7 => [6, 8],
-      8 => [7, 9, 18],
-      9 => [8, 10],
-      10 => [9],
-      12 => [2, 13],
-      13 => [12],
-      14 => [4, 15],
-      15 => [14],
-      16 => [6, 17],
-      17 => [16],
-      18 => [8, 19],
-      19 => []
-    }
-  }
 
   @energy_consumption %{
     0 => 1,
@@ -122,60 +36,46 @@ defmodule AdventOfCode.Day23 do
     IO.puts("")
   end
 
-  def possible_move([a1, a2, b1, b2, c1, c2, d1, d2] = positions, {who, from, to}) do
-    occupied_cells = MapSet.new(positions)
-    open_corridor_a = not MapSet.member?(occupied_cells, 12) and (a1 == 13 or a2 == 13)
-    open_corridor_b = not MapSet.member?(occupied_cells, 14) and (b1 == 15 or b2 == 15)
-    open_corridor_c = not MapSet.member?(occupied_cells, 16) and (c1 == 17 or c2 == 17)
-    open_corridor_d = not MapSet.member?(occupied_cells, 18) and (d1 == 19 or d2 == 19)
-
+  def opened_hallway(hallway, occupied, inverse) do
+    high = 12 + 2 * hallway
+    low = high + 1
+    # le hallway est ouvert dans deux cas: soit les deux cases sont vides, soit celle d'en haut est vide et celle d'en bas remplie par le bon type d'amphipod
     cond do
-      # Pods able to enter their corridor => priority
-      to == 2 and (who == 0 or who == 1) and open_corridor_a ->
-        [{who, to, 12}]
-
-      to == 4 and (who == 2 or who == 3) and open_corridor_b ->
-        [{who, to, 14}]
-
-      to == 6 and (who == 4 or who == 5) and open_corridor_c ->
-        [{who, to, 16}]
-
-      to == 8 and (who == 6 or who == 7) and open_corridor_d ->
-        [{who, to, 18}]
-
-      # pods cant stay
-      to == 2 or to == 4 or to == 6 or to == 8 ->
-        [{who, to, to - 1}, {who, to, to + 1}]
-
-      true ->
-        for {pos, pod} <- with_index(positions),
-            do: for(dest <- @maze[div(pod, 2)][pos], do: {pod, pos, dest})
+      not MapSet.member?(occupied, high) and not MapSet.member?(occupied, low) -> true
+      not MapSet.member?(occupied, high) and Map.get(inverse, low) == hallway -> true
+      true -> false
     end
-    |> List.flatten()
-    |> filter(fn {_pod, _origin, dest} = move ->
-      not (MapSet.member?(occupied_cells, dest) or move == {who, to, from})
-    end)
+
+  end
+  def possible_move([a1, a2, b1, b2, c1, c2, d1, d2] = positions) do
+    occupied_cells = MapSet.new(positions)
+    inverse_pos = with_index(positions) |> Enum.map(fn {k,v} -> {k, div(v,2)} end) |> Map.new()
+
+    hallways = for h <- 0..3, into: %{}, do: {h, opened_hallway(h, occupied_cells, inverse_pos)}
+
+    |> IO.inspect()
+
   end
 
-  def explore({_state, _last_move, best_score, _moves, _energy, _win}, level) when level > 1,
+  def explore({_state, best_score, _moves, _energy, _win}, level) when level > 1,
     do: best_score
 
-  def explore({state, last_move, best_score, moves, energy, win}, level) do
+  def explore({state, best_score, moves, energy, win}, level) do
     print(state)
-    p_moves = possible_move(state, last_move)
+    p_moves = possible_move(state)
 
     for {who, _from, to} = move <- p_moves do
       new_state = List.update_at(state, who, fn _ -> to end)
       # IO.inspect(move)
       new_energy = energy + @energy_consumption[div(who, 2)]
-      new_s = {new_state, move, best_score, [move | moves], new_energy, win}
+      new_s = {new_state, best_score, [move | moves], new_energy, win}
       explore(new_s, level + 1)
     end
   end
 
   def part1(_args) do
     start = [13, 19, 12, 16, 14, 17, 15, 18]
-    explore({start, {nil, -1, -1}, @infinite, [], 0, false}, 0)
+    explore({start, @infinite, [], 0, false}, 0)
     :ok
   end
 
